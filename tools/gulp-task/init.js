db4a2e90ec09a23603df617d8f26bf2d3dbb4435
Ivan Koretskiy <gillbeits@gmail.com>
2016-03-16 13:13:56 +0300

    fix

--- tools/gulp-task/init.js
+++ tools/gulp-task/init.js
@@ -6,7 +6,7 @@ module.exports = function (gulp, $) {
 
     gulp.task('init', function () {
         var yargs = require('yargs');
-        return gulp.src('template/**/*')
+        return gulp.src($._path.join(__dirname + '/../../template/**/*'))
             .pipe(gulp.dest($._path.join(process.cwd(), yargs.argv.dir || '')))
         ;
     });
